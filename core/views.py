8bfa1559f7a0f1802b089b1345aed0054620a3ce
Oscar Byiringiro <byiringoroscar@gmail.com>
2024-04-09 16:31:11 +0200

    Redirect with htmx

--- core/views.py
+++ core/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
+from django_htmx.http import HttpResponseClientRedirect
 
 # Create your views here.
 
 
 def index(request):
     if request.htmx:
-        return render(request, 'partial.html')
+        return HttpResponseClientRedirect('/admin')
+        # return render(request, 'partial.html')
     return render(request, 'index.html')
