d3545bda8f180d910b6fe9c85b106a08cd7d11a6
Oscar Byiringiro <byiringoroscar@gmail.com>
2024-04-09 17:17:05 +0200

    Use beforend with reswap

--- core/views.py
+++ core/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django_htmx.http import HttpResponseLocation, HttpResponseStopPolling, push_url
+from django_htmx.http import HttpResponseLocation, HttpResponseStopPolling, push_url, reswap
 from django.http import HttpResponse
 import random
 # Create your views here.
@@ -8,8 +8,10 @@ import random
 def index(request):
     if request.htmx:
         # return HttpResponseLocation('/success/', target="#htmx-content")
-        response = render(request, 'partial.html')
-        return push_url(response, '/lorem/')
+        # response = render(request, 'partial.html')
+        # return push_url(response, '/lorem/')
+        response  = render(request, 'partial.html')
+        return reswap(response, 'beforeend')
     return render(request, 'index.html')
 
 
