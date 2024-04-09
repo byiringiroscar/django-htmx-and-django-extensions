f942d20d283063232f9bbbc6eef44a01b5c3b1c3
Oscar Byiringiro <byiringoroscar@gmail.com>
2024-04-09 16:54:39 +0200

    Response location

--- core/views.py
+++ core/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render
-from django_htmx.http import HttpResponseClientRedirect
+from django_htmx.http import HttpResponseLocation, HttpResponseStopPolling
+from django.http import HttpResponse
 
 # Create your views here.
 
 
 def index(request):
     if request.htmx:
-        return HttpResponseClientRedirect('/admin')
+        return HttpResponseLocation('/success/', target="#htmx-content")
         # return render(request, 'partial.html')
     return render(request, 'index.html')
+
+
+def success(request):
+    return HttpResponse('Success always!')
